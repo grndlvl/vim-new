6867c64a69204bf18b5487fbbb18373f629e5643
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2014-04-06 21:27:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Dotfiles
+========
+
+Installation
+------------
+
+* Fork and clone this git repo.
+* Run `./install.sh` (you may need to `chmod +x`).
+
+Originally forked from: [theunraveler](https://github.com/theunraveler/dotfiles)
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+WD="$(pwd)"
+DIR="$( cd "$(dirname "$0")/" && pwd )"
+
+# First, initialize git submodules.
+git submodule init; git submodule update;
+
+cd ~;
+echo "Linking items from $DIR to home directory using $(which ln)"
+
+# If we are using GNU ln (from coreutils) the options are different.
+ln --version > /dev/null
+if [ $? -eq 0 ]; then
+  ln_command="ln -ns"
+else
+  ln_command="ln -hs"
+fi
+
+# Then, symlink all dotfiles into the home directory.
+for filepath in `find "$DIR" ! -path "*.git*" -name "*.symlink"`; do
+  filename=$(basename "${filepath%.*}")
+  echo -n "Linking $filename..."
+
+  $ln_command "$filepath" ".$filename"
+
+  if [ $? -eq 0 ]; then echo "done"; fi
+done
+
+# Finally, go back to where the user started.
+cd $WD
+exit 0
